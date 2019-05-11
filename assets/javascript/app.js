c3c4257eb4ab1c2c0360a97b7485931573cebe89
Jeffrey Fleer <Jeff@as-MacBook-Pro.local>
2019-05-10 20:19:16 -0600

    added feature to update Minutes Away every minute

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -49,6 +49,9 @@ var firebaseConfig = {
         // Calculate the time to the next train
         // Create a moment object with the current time
 
+        // ==================== IT WORKS!!!! =========================
+        //======================
+
         var trainMoment = moment(firstTime, "hh:mm");
         // Calculate minutes from start time until next arrival
         var currentTime = moment().diff(moment(trainMoment), "m");
@@ -65,8 +68,8 @@ var firebaseConfig = {
         var TDname = $("<td>").html(tName);
         var TDdestination = $("<td>").html(destination);
         var TDfrequency = $("<td>").html(frequency + " (mins)");
-        var TDarrivalTime = $("<td>").html(nextArrival);
-        var TDminAway = $("<td>").html(minsAway);
+        var TDarrivalTime = $("<td>").html(nextArrival).attr("id", tName);
+        var TDminAway = $("<td>").html(minsAway).attr("id", frequency);
         $(row).append(TDname, TDdestination, TDfrequency, TDarrivalTime, TDminAway);
         $("#table").append(row);
     })
@@ -85,5 +88,35 @@ var firebaseConfig = {
     function arrivalTime(){
         partTime = moment().format('h:mm:ss a');
     }
+
+    // Create a function that will fetch FireBase data and update arrival times every 30 seconds
+    function updateArrivalTime(){
+        return firebase.database().ref().once('value').then(function(snapshot){
+            var snapshot = snapshot.val();
+            $.each(snapshot, function(key, value){
+
+                var firstTime = value.firstTime;
+                var frequency = value.frequency;
+                var tName = value.tName;
+    
+                var trainMoment = moment(firstTime, "hh:mm:ss");
+
+                // Calculate minutes from start time until next arrival
+                var currentTime = moment().diff(moment(trainMoment), "m");
+
+                var minsAway = frequency - (currentTime % frequency);
+                if (minsAway === parseInt(frequency)){
+                    minsAway = 0;
+                }
+                // Use the calculated times to display the time of the next arrival
+                var nextArrival = moment().add(minsAway, "m").format("h:mm a");
+    
+                // Update the relevant table elements
+                $("#"+tName).empty().append(nextArrival);
+                $("#"+frequency).empty().append(minsAway);
+            })
+        })
+    }
+    var updateArrivals = setInterval(updateArrivalTime, 1000)
     
     // })
